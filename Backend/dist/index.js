8e9be8045a081f7f162d448ab851ec372f5a2cfc
Nich Galzin <74174344+nichgalzin@users.noreply.github.com>
2024-06-10 17:17:15 +0100

    Backend update

--- Backend/dist/index.js
+++ Backend/dist/index.js
@@ -7,9 +7,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const objectRoutes_1 = __importDefault(require("./routes/objectRoutes"));
+const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use("/api/", objectRoutes_1.default);
 app.get("/", (req, res) => {

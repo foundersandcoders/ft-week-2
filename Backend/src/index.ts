a602ea9f1fae587afc76bd40cc8bc344049e25c3
Nich Galzin <74174344+nichgalzin@users.noreply.github.com>
2024-06-12 12:49:47 +0100

    removed home route

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -14,11 +14,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/", objectRoutes)
 
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
-
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
